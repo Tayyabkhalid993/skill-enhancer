5237f5f91a9d74df7d224a7e7d9aa2de010f5309
tayyabkhalid993 <tayyabkhalid993@gmail.com>
2024-10-12 12:10:40 +0500

    setting up sanity

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <div className="bg-light rounded-lg mb-2 text-gray-700 text-center p-4 sm:flex sm:flex-row-reverse sm:justify-around items-center lg:justify-between lg:px-8">
-            <div className="flex justify-center">
+            <div className="flex justify-center relative">
                 <div className="p-2"><a href={'/'}>About Us</a></div>
                 <div className="p-2"><a href={'/'}>Contact Us</a></div>
                 <div className="p-2"><a href={'/'}>Disclaimer</a></div>
-                <button className="border px-2 rounded-lg scroll-smooth">
-                    <Link href={'#top'}><ArrowUp/></Link>
+                <button className="hidden  sm:block border px-2 rounded-lg scroll-smooth">
+                    <Link href={'/'}><ArrowUp/></Link>
                 </button>
             </div>
             <div className="m-3">
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import scholarship from './scholarship'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [scholarship],
 }
